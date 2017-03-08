8b44216227ac3c511d1828f51917e5c14caa54fb
Thomas SAUVAJON <thomas.sauvajon@gmail.com>
2017-03-08 16:11:49 +0100

    reducer for action ADD_TODO

---
+++ src/todos.test.js
@@ -0,0 +1,25 @@
+import todos from './todos';
+import deepFreeze from 'deep-freeze';
+
+it('adds a todo', () => {
+  const stateBefore = [];
+  const action = {
+    type: 'ADD_TODO',
+    id: 0,
+    text: 'Learn Redux',
+  };
+  const stateAfter = [
+    {
+      id: 0,
+      text: 'Learn Redux',
+      completed: false,
+    }
+  ];
+
+  deepFreeze(stateBefore);
+  deepFreeze(action);
+
+  expect(
+    todos(stateBefore, action)
+  ).toEqual(stateAfter);
+});
